96e6774a414010aa30dc668c425217881b493321
dave doolin <david.doolin@gmail.com>
2021-02-05 05:07:04 -0800

    video 109 change to demo workflow

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - today! Yes!
+          Video 109
         </a>
       </header>
     </div>
